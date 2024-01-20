7ec0249937e1753d885267fa77fd21a970524588
Alex Lima <alex.lima_dev@outlook.com>
2024-01-20 12:35:01 -0300

    <feat>(controllers/checkout): Controller show para exibir um pedido concluído especifico através do seu ID e os itens de compra desse pedido especifico.

--- src/controllers/checkoutControllers.js
+++ src/controllers/checkoutControllers.js
@@ -4,7 +4,14 @@ const AppError = require("../utils/appError")
 class CheckoutControllers {
   async create(request, response) {
     const user_id = request.user.id
-    const { paymentType, payment, deliveryType, orderCompleted, numberInstallments, cartItems } = request.body
+    const {
+      paymentType,
+      payment,
+      deliveryType,
+      orderCompleted,
+      numberInstallments,
+      cartItems,
+    } = request.body
 
     const [order_id] = await knex("orders").insert({
       user_id,
@@ -20,7 +27,7 @@ class CheckoutControllers {
         order_id,
         user_id,
         dish_id: item.id,
-        count: item.count
+        count: item.count,
       }
     })
 
@@ -29,9 +36,69 @@ class CheckoutControllers {
     response.json({
       message: `Pedido número ${order_id} criado com sucesso`,
     })
-
   }
 
+  async index(request, response) {
+    const { role, id } = request.user
+
+    if (role === "customer") {
+      const orders = await knex("orders").where({ user_id: id }).orderBy("id")
+      const itemsOrder = await knex("orderItems").where({ user_id: id })
+
+      const ordersWithItemsOrder = orders.map((order) => {
+        const orderItems = itemsOrder.filter(
+          (item) => item.order_id === order.id
+        )
+        return {
+          ...order,
+          itemsOrder: orderItems,
+        }
+      })
+
+      if (ordersWithItemsOrder.length === 0) {
+        throw new AppError("Não foi possível localizar os pedidos.")
+      }
+
+      return response.json(ordersWithItemsOrder)
+    } else if (role === "admin") {
+      const orders = await knex("orders").orderBy("id")
+      const itemsOrder = await knex("orderItems")
+
+      const ordersWithItemsOrder = orders.map((order) => {
+        const orderItems = itemsOrder.filter(
+          (item) => item.order_id === order.id
+        )
+        return {
+          ...order,
+          itemsOrder: orderItems,
+        }
+      })
+
+      if (ordersWithItemsOrder.length === 0) {
+        throw new AppError("Não foi possível localizar os pedidos.")
+      }
+
+      return response.json(ordersWithItemsOrder)
+    }
+  }
+
+  async show(request, response) {
+    const { id } = request.params
+
+    const order = await knex("orders").where({ id })
+    const itemsOrder = await knex("orderItems").where({ order_id: id })
+
+    if (order.length <= 0) {
+      throw new AppError("Pedido não encontrado", 401)
+    }
+
+    const orderAndItemsOrder = {
+      order,
+      itemsOrder
+    }
+
+    return response.json(orderAndItemsOrder)
+  }
 }
 
 module.exports = new CheckoutControllers()
--- src/routes/checkout.routes.js
+++ src/routes/checkout.routes.js
@@ -2,8 +2,12 @@ const { Router } = require("express")
 const checkoutRoutes = Router()
 
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
+const verifyUserAuthorization = require("../middlewares/verifyUserAuthorization")
+
 const checkoutControllers = require("../controllers/checkoutControllers")
 
-checkoutRoutes.post("/", ensureAuthenticated, checkoutControllers.create)
+checkoutRoutes.post("/", ensureAuthenticated, verifyUserAuthorization(["customer"]), checkoutControllers.create)
+checkoutRoutes.get("/", ensureAuthenticated, verifyUserAuthorization(["customer", "admin"]), checkoutControllers.index)
+checkoutRoutes.get("/:id", ensureAuthenticated, verifyUserAuthorization(["customer", "admin"]), checkoutControllers.show)
 
 module.exports = checkoutRoutes
